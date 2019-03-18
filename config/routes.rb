52e605998271356d29ec9e9a066e8d3c51fa03ee
Victor Rubio <vctrubio@gmail.com>
2019-03-18 17:28:11 -0600

    controller for reservation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
 
-resources :restaurants, only: [:index, :show, :new, :create]
+  # get 'reservations/new'
+  # get 'reservations/index'
+  # get 'reservations/show'
+  
+resources :restaurants, only: [:index, :show, :new, :create] do 
+  resources :reservations, only: [:new, :create]
+end
  
 devise_for :users 
   resources :users  do
@@ -9,8 +15,8 @@ devise_for :users
      get :restaurants, to: "restaurants"
     end
   end
-
-
+  
+  
    root "restaurants#index"
   #FOR THE FUTURE ADD ROOOT HOMEPAGE (FIGMA)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
