9952b35cefa40cfcecaabe8dab1f1558e5cd9a6c
Michael Villarreal <mvillarreal0605@gmail.com>
2019-03-19 13:17:12 -0600

    Fix

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,6 +3,7 @@ class RestaurantsController < ApplicationController
 
   def index
     @restaurants = Restaurant.all
+    @user = current_user
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         #:omniauthable, :lockable
   has_many :restaurants, dependent: :destroy
-  
+
   validates :username, presence: true, uniqueness: true
   validates :email, presence: true #REGEX TO CHECK IF TRUE
+
+
 
 end
