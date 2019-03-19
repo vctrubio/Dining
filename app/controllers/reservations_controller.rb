957484a9c249352180417d8af7adf54903c85ffd
Michael Villarreal <mvillarreal0605@gmail.com>
2019-03-19 13:46:26 -0600

    Devise SignUp with Username

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,5 +1,9 @@
 class ReservationsController < ApplicationController
+  before_action :find_restaurant, only:[:new, :create]
+
+
   def new
+    @reservation = Reservation.new
   end
 
   def index
@@ -10,4 +14,20 @@ class ReservationsController < ApplicationController
 
   def create
   end
+
+  private
+
+  def reservation_params
+    params.require(:reservation).permit(:date, :user_id)
+  end
+
+  def find_reservation
+    @reservation = Reservation.find(params[:id])
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+
 end
