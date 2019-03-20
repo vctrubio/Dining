2a25e5fdab29c7b41392f3ab6e0775780709c067
Victor Rubio <vctrubio@gmail.com>
2019-03-20 15:45:23 -0600

    become a chef button link route update path finished

---
+++ app/policies/user_policy.rb
@@ -0,0 +1,17 @@
+class UserPolicy < ApplicationPolicy
+    class Scope < Scope
+      def resolve
+        scope.all
+      end
+    end
+  
+    def show?
+     true
+    end
+      
+    def update?
+      true
+    end
+  end
+  
+  
