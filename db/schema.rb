a6cda298278073b58fb6b6f80afe6222808ca59b
Catharina Komrij <komcath@gmail.com>
2019-03-20 17:51:21 -0600

    fishdciud

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_20_185443) do
-
+ActiveRecord::Schema.define(version: 2019_03_20_202028) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
