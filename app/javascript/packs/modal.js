7a384c5e582fc837e9ab6b9a59481226a9ec5c61
oreoorbitz <oreoorbitz@gmail.com>
2019-03-22 13:40:58 -0600

    properly fixed the modal issue

--- app/javascript/packs/modal.js
+++ app/javascript/packs/modal.js
@@ -2,6 +2,7 @@ const modal = document.getElementById('simple-modal')
 const modalButton = document.getElementById('modal-trigger')
 const modalClose = document.getElementById('close')
 
+if ( modalButton != null) {
 const openModal = () => modal.style.display = 'block';
 
 modalButton.addEventListener('click', openModal);
@@ -20,3 +21,4 @@ const outsideClick = (e) => {
 };
 
 window.addEventListener('click', outsideClick);
+};
