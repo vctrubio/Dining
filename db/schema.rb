717c451d5b764600ad8b7c85b5a1f1b20e7740ac
oreoorbitz <oreoorbitz@gmail.com>
2019-04-30 07:44:16 -0500

    added a desccription field to the database, then added option to add and edit description in the view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_24_183532) do
+ActiveRecord::Schema.define(version: 2019_04_27_174739) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2019_04_24_183532) do
     t.string "photo", default: "https://res.cloudinary.com/vvrruubb69e/image/upload/v1553290806/photo-1521017432531-fbd92d768814.jpg"
     t.time "open_hour"
     t.time "close_hour"
+    t.text "description"
     t.index ["user_id"], name: "index_restaurants_on_user_id"
   end
 
